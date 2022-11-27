b8bc06e542427105cdf4e2674f899195f81d04d6
Haline Ferreira <Haline_Ferreira@Dell.com>
2022-11-27 15:34:56 -0300

    Make subjectBanner responsive

--- ilearn-ui/ilearn-ui/ClientApp/src/pages/Home/index.js
+++ ilearn-ui/ilearn-ui/ClientApp/src/pages/Home/index.js
@@ -23,6 +23,32 @@ export class Home extends Component {
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
+      responsive: [
+        // {
+        //   breakpoint: 1024,
+        //   settings: {
+        //     slidesToShow: 3,
+        //     slidesToScroll: 3,
+        //     infinite: true,
+        //     dots: true,
+        //   },
+        // },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     }
 
     return (
